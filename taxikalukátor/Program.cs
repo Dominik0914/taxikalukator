33275dd72fe878fc841885e019fbd57e1ceef7cb
Dominik0914 <rozsa.dominik.2024d@telekimezotur.hu>
2026-09-17 12:41:41 +0200

    2+3.feladat

--- taxikalukátor/Program.cs
+++ taxikalukátor/Program.cs
@@ -1,2 +1,23 @@
-﻿Console.WriteLine("kérem az utas nevét:");
+﻿///2.feladat
+Console.WriteLine("kérem az utas nevét:");
 string utasnev = Console.ReadLine();
+Console.WriteLine("megtett út?(egész szám,km):");
+int ut = int.Parse(Console.ReadLine());
+bool esti=false;
+Console.WriteLine("esti utazás volt?(igen/nem):");
+string estiValasz = Console.ReadLine();
+if (estiValasz == "igen")
+{
+    esti = true;
+}
+Console.WriteLine("alapdíj(egész szám):");
+int dij = int.Parse(Console.ReadLine());
+Console.WriteLine("kilometer díj:");
+int kildij = int.Parse(Console.ReadLine());
+double eszakaipotlek = 1.20;
+///3.feladat
+int osszeg = ut*kildij+dij;
+if (esti==true)
+{
+    osszeg = (int)(osszeg * eszakaipotlek);
+}
